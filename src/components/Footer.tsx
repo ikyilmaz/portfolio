67190eb08d2c7a0e2e1a13cc3866bbb7a43c59e4
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2022-03-31 02:56:33 +0300

    delete unused imports

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
-import { landingFadeIn } from "../utils/fadeInUp";
 import { gsap, Cubic } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const Wrapper = styled.div`
   .collab {
