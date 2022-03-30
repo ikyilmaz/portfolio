3cd63fdb022651e65d1f188a910f126efeb9c474
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2022-03-31 00:21:38 +0300

    add timeline

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,12 +3,14 @@ import { Footer } from "../components/Footer";
 import { Hero } from "../components/Hero";
 import { NavigationBar } from "../components/NavigationBar/NavigationBar";
 import { Projects } from "../components/Projects";
+import { Timeline } from "../components/Timeline";
 
 export const HomePage: React.FC = () => {
   return (
     <React.Fragment>
       <NavigationBar />
       <Hero />
+      <Timeline />
       <Projects />
       <Footer />
     </React.Fragment>
