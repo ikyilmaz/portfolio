071c7d338f579eb247ab28aeb874a5dfcc747956
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2022-04-05 01:27:18 +0300

    add hr class

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     width: 100%;
     height: 100%;
-    font-family: "Roboto Mono" !important;
+    font-family: "JetBrains Mono" !important;
     background: #0f0f0f !important;
     color: #fff !important;
   }
@@ -22,6 +22,11 @@ const GlobalStyle = createGlobalStyle`
   .whitespace {
     width: 100%;
     height: 100px;
+  }
+  
+  .hr {
+    background: rgba(255, 255, 255, 0.2);
+    height: 1px;
   }
 
   @media(max-width: 768px) {
