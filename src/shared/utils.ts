ded244b983f4876c89308718c5fc848e912b68ab
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2022-04-05 01:28:02 +0300

    add fadeIn landing animation func

---
+++ src/shared/utils.ts
@@ -0,0 +1,29 @@
+import { gsap, Cubic } from "gsap";
+import React from "react";
+
+type FadeInOpts = {
+  elRefs: React.RefObject<HTMLElement>[];
+  from?: string;
+  stagger?: number;
+};
+
+export const fadeIn = ({ elRefs, from = "left", stagger }: FadeInOpts) => {
+  const els = elRefs.map((el) => el.current);
+
+  gsap.from(els, {
+    duration: 1,
+    // delay: 0.4,
+    y: (() => {
+      if (from === "bottom") return 100;
+      if (from === "top") return -100;
+    })(),
+
+    x: (() => {
+      if (from === "right") return 100;
+      if (from === "left") return -100;
+    })(),
+    opacity: 0,
+    stagger,
+    ease: Cubic.easeInOut,
+  });
+};
