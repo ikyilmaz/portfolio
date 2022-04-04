cb0c5ba6dc6b20420551f09731c0d7bea912a329
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2022-04-05 01:54:06 +0300

    add custom scrollbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import styled, { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from "styled-components";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { AboutPage } from "./pages/AboutPage";
 import { ContactPage } from "./pages/ContactPage";
 
-const Wrapper = styled.div``;
-
 const GlobalStyle = createGlobalStyle`
+  /* ===== Scrollbar CSS ===== */
+  /* Firefox */
+  * {
+    scrollbar-width: auto;
+    scrollbar-color: #ffffff #ffffff;
+  }
+
+  /* Chrome, Edge, and Safari */
+  *::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  *::-webkit-scrollbar-track {
+    background: #0f0f0f;
+  }
+
+  *::-webkit-scrollbar-thumb {
+    background-color: #ffffff;
+    border-radius: 2px;
+    border: 2px solid #000000;
+  }
+
   html, body {
     margin: 0;
     padding: 0;
