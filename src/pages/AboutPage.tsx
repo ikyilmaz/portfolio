389ce3299c838d7ae45fc174736c699d64f43635
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2022-04-05 00:39:58 +0300

    add my photo

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import { Footer } from "../components/Footer";
 import { NavigationBar } from "../components/NavigationBar/NavigationBar";
+import { Timeline } from "../components/Timeline";
+import ben from "../img/ben.jpeg";
 
 const Wrapper = styled.div`
   .about {
@@ -10,8 +12,8 @@ const Wrapper = styled.div`
   }
 
   .image {
-    background: url(https://images.unsplash.com/photo-1540885762261-a2ca01f290f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2468&q=80)
-      no-repeat 50% 70%;
+    background: url(${ben}) no-repeat 50% 0%;
+    filter: grayscale(1);
   }
 `;
 
@@ -35,29 +37,9 @@ export const AboutPage: React.FC = () => {
 
             <div className="row">
               <div className="col-lg-12">
-                <h3 className="wow fadeInUp" data-wow-delay="1.2s">
-                  hakkımda.
-                </h3>
                 <br />
-
-                <p>
-                  Üniversiteyi Akdeniz Üniversitesi Bilgisayar Programcılığı
-                  Bölümünde okudum. Bu dönemde kendimi <b>web developer</b>{" "}
-                  olarak geliştirmeye çalıştım. Front-End alanında ilk
-                  temellerimi <b>(HTML-Pug/CSS-SASS-LESS/JS-TS)</b> attıktan
-                  sonra <b>VueJS</b> kütüphanesiyle ve ardından sırasıyla{" "}
-                  <b>ReactJS</b>, <b>Angular</b> kütüphanelerini kullanarak
-                  projeler geliştirdim. Back-end kısmında ise ağırlıklı olarak{" "}
-                  <b>NodeJS</b> üzerinde geliştirmeler yaptım. NodeJS
-                  kullanırken de <b>NestJS</b> ve <b>ExpressJS</b>{" "}
-                  kütüphanelerinden yararlandım. Veritabanı olarak{" "}
-                  <b>PostgreSQL</b>, <b>MySQL</b>, <b>MongoDB</b> ve{" "}
-                  <b>Redis</b> kullanmayı amaçladım. ORM/ODM olarak{" "}
-                  <b>Sequelize</b>, <b>typeorm</b> ve <b>mongoose</b> kullanmayı
-                  öğrendim. <b>GoLang</b>'e merak salıp, öğrendim. <b>Gin</b> ve{" "}
-                  <b>gofiber</b> kütüphanelerini kullanarak back-end yazılımlar
-                  geliştirdim.
-                </p>
+                <br />
+                <Timeline />
               </div>
             </div>
           </div>
